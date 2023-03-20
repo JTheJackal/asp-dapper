f7d4bd6bbac526edeba5ca14df6cc15db0f8d105
Joshua <styles@outlook.com>
2023-03-20 11:58:48 +0000

    APIs added for Status and Role

---
+++ asp-dapper/asp-dapper/Controllers/RoleController.cs
@@ -0,0 +1,89 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace asp_dapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+        public RoleController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        [HttpGet]
+        [Route("All")]
+        public async Task<ActionResult<List<Role>>> GetAllRoles()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<Role> Roles = await SelectAllRoles(connection);
+
+            return Ok(Roles);
+        }
+
+
+        [HttpGet("Get/{RoleId}")]
+        public async Task<ActionResult<Role>> GetRole(int RoleId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            Role Role = await SelectSingleRole(RoleId, connection);
+
+            return Ok(Role);
+        }
+
+
+        [HttpPost]
+        [Route("Add")]
+        public async Task<ActionResult<List<Role>>> AddRole(Role Role)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("INSERT INTO tblROLE (name) VALUES (@Name)", Role);
+
+            return Ok(await SelectAllRoles(connection));
+        }
+
+
+        [HttpPut]
+        [Route("Edit")]
+        public async Task<ActionResult<List<Role>>> EditRole(Role Role)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("UPDATE tblROLE SET Name = @Name WHERE Id = @Id", Role);
+
+            return Ok(await SelectSingleRole(Role.Id, connection));
+        }
+
+
+        [HttpDelete("Delete/{RoleId}")]
+        public async Task<ActionResult<List<Role>>> DeleteRole(int RoleId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("DELETE FROM tblROLE WHERE ID = @Id", new
+            {
+                Id = RoleId
+            });
+
+            return Ok(await SelectAllRoles(connection));
+        }
+
+
+        // Return all Roles
+        private static async Task<IEnumerable<Role>> SelectAllRoles(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Role>("select * from tblROLE");
+        }
+
+        // Return a single Role with a specified ID
+        private static async Task<Role> SelectSingleRole(int RoleId, SqlConnection connection)
+        {
+            return await connection.QueryFirstAsync<Role>("select * from tblROLE where id = @Id",
+                            new { Id = RoleId });
+        }
+    }
+}
---
+++ asp-dapper/asp-dapper/Controllers/StatusController.cs
@@ -0,0 +1,89 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace asp_dapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+        public StatusController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        [HttpGet]
+        [Route("All")]
+        public async Task<ActionResult<List<Status>>> GetAllStatuses()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<Status> statuses = await SelectAllStatuses(connection);
+
+            return Ok(statuses);
+        }
+
+
+        [HttpGet("Get/{statusId}")]
+        public async Task<ActionResult<Status>> GetStatus(int statusId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            Status status = await SelectSingleStatus(statusId, connection);
+
+            return Ok(status);
+        }
+
+
+        [HttpPost]
+        [Route("Add")]
+        public async Task<ActionResult<List<Status>>> AddStatus(Status status)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("INSERT INTO tblSTATUS (name) VALUES (@Name)", status);
+
+            return Ok(await SelectAllStatuses(connection));
+        }
+
+
+        [HttpPut]
+        [Route("Edit")]
+        public async Task<ActionResult<List<Status>>> EditStatus(Status status)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("UPDATE tblSTATUS SET Name = @Name WHERE Id = @Id", status);
+
+            return Ok(await SelectSingleStatus(status.Id, connection));
+        }
+
+
+        [HttpDelete("Delete/{statusId}")]
+        public async Task<ActionResult<List<Status>>> DeleteStatus(int statusId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("DELETE FROM tblSTATUS WHERE ID = @Id", new
+            {
+                Id = statusId
+            });
+
+            return Ok(await SelectAllStatuses(connection));
+        }
+
+
+        // Return all statuses
+        private static async Task<IEnumerable<Status>> SelectAllStatuses(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Status>("select * from tblSTATUS");
+        }
+
+        // Return a single status with a specified ID
+        private static async Task<Status> SelectSingleStatus(int statusId, SqlConnection connection)
+        {
+            return await connection.QueryFirstAsync<Status>("select * from tblSTATUS where id = @Id",
+                            new { Id = statusId });
+        }
+    }
+}
